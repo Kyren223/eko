6b2a93eadd54b1b16028f0e95faabc9cb2184445
Kyren223 <ulmliad223@gmail.com>
2024-10-10 01:15:59 +0300

    fix: delete removed packets package

--- internal/server/handler.go
+++ internal/server/handler.go
@@ -6,7 +6,6 @@ import (
 	"sync"
 
 	"github.com/kyren223/eko/internal/utils/log"
-	"github.com/kyren223/eko/internal/utils/packets"
 )
 
 func handleConnection(conn net.Conn, wg *sync.WaitGroup) {
@@ -14,24 +13,6 @@ func handleConnection(conn net.Conn, wg *sync.WaitGroup) {
 	defer log.Info("Disconnecting client: %v", conn.RemoteAddr().String())
 	defer conn.Close()
 	defer wg.Done()
-
-	// packet, err := packets.ReadPacket(conn)
-	// if err != nil {
-	// 	log.Info("Received bad packet: %v", err)
-	// 	err = respondWithError(conn, err, 0)
-	// 	if err != nil {
-	// 		log.Error("Failed to respond to bad packet: %v", err)
-	// 	}
-	// 	return
-	// }
-	//
-	// if ok, reason := isPacketOk(packet); !ok {
-	// 	err = respondWithError(conn, fmt.Errorf("unsupported packet: %v", reason), 0)
-	// 	if err != nil {
-	// 		log.Error("Failed to respond to unsupported packet: %v", err)
-	// 	}
-	// 	return
-	// }
 
 	buffer := make([]byte, 1024)
 	bytesRead, err := conn.Read(buffer)
@@ -51,24 +32,3 @@ func handleConnection(conn net.Conn, wg *sync.WaitGroup) {
 	log.Info("Written %v bytes", bytesWritten)
 }
 
-func isPacketOk(packet packets.Packet) (bool, string) {
-	if packet.Version() != packets.V1 {
-		return false, "versions other than 1 are not supported"
-	}
-	if packet.HasFlag(packets.FlagError) {
-		return false, "error flag should only be used by the server"
-	}
-	return true, ""
-}
-
-func respondWithError(conn net.Conn, err error, extraFlag byte) error {
-	flag := packets.FlagError | extraFlag
-	packet := packets.NewPacket(packets.V1, flag, []byte(err.Error()))
-	return packet.Write(conn)
-}
-
-func processPacket(packet packets.Packet) (sessId uint16, data []byte, err error) {
-	if packet.HasFlag(packets.FlagHandshake) {
-	}
-	return
-}
