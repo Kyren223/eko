38f0cd771ed1a9029f6cdd5532e5c88d9c1c3b71
Kyren223 <Kyren223@proton.me>
2024-11-22 19:14:23 +0200

    Some more prototypes on the design

--- internal/client/ui/networks/networks.go
+++ internal/client/ui/networks/networks.go
@@ -1,19 +1,20 @@
 package networks
 
 import (
-	"fmt"
 	"strings"
 
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
+	"github.com/kyren223/eko/internal/client/ui"
+	"github.com/kyren223/eko/internal/data"
 )
 
 var (
 	style        = lipgloss.NewStyle().Border(lipgloss.ThickBorder(), true, false)
-	networkStyle = lipgloss.NewStyle().Width(6).Height(3).PaddingTop(1).
+	networkStyle = lipgloss.NewStyle().Width(6).Height(3).PaddingTop(1).Margin(0, 1, 1).
 			Align(lipgloss.Center).
 			Foreground(lipgloss.Color("#ffbf00")).Background(lipgloss.Color("#4a3d5c")).
-			Border(lipgloss.ThickBorder(), false, true)
+			Border(lipgloss.ThickBorder(), false, false)
 	networks = []string{
 		"󰜈 ",
 		" ",
@@ -26,10 +27,18 @@ var (
 		// "Hmm",
 		// "Another",
 	}
+
+	label = lipgloss.NewStyle().
+		Background(lipgloss.Color("#20999D")).Foreground(lipgloss.Color("#FFFFFF")).
+		Render("Dev")
+	name = lipgloss.NewStyle().
+		Render("Kyren223")
+	user = lipgloss.JoinHorizontal(lipgloss.Center, label, " ", name)
 )
 
 type Model struct {
 	networks []string
+	a        data.Network
 }
 
 func New() Model {
@@ -43,22 +52,30 @@ func (m Model) Init() tea.Cmd {
 }
 
 func (m Model) View() string {
-	border := lipgloss.ThickBorder()
+	// border := lipgloss.ThickBorder()
 	var builder strings.Builder
 
-	top := strings.Repeat(border.Top, 6)
-	builder.WriteString(fmt.Sprintf("%s%s%s\n", border.TopLeft, top, border.TopRight))
+	// top := strings.Repeat(border.Top, 6)
+	// builder.WriteString(fmt.Sprintf("%s%s%s\n", border.TopLeft, top, border.TopRight))
+	builder.WriteString("\n")
 	for i, network := range m.networks {
 		if i != 0 {
-			middle := strings.Repeat(border.Top, 6)
-			builder.WriteString(fmt.Sprintf("\n%s%s%s\n", border.MiddleLeft, middle, border.MiddleRight))
+			// middle := strings.Repeat(border.Top, 6)
+			// builder.WriteString(fmt.Sprintf("\n%s%s%s\n", border.MiddleLeft, middle, border.MiddleRight))
+			// middle := strings.Repeat(" ", 8)
+			// builder.WriteString(fmt.Sprintf("\n%s\n", middle))
 		}
-		builder.WriteString(networkStyle.Render(network))
+		builder.WriteString(networkStyle.Render(network) + "\n")
 	}
-	bottom := strings.Repeat(border.Bottom, 6)
-	builder.WriteString(fmt.Sprintf("\n%s%s%s", border.BottomLeft, bottom, border.BottomRight))
+	// bottom := strings.Repeat(border.Bottom, 6)
+	// builder.WriteString(fmt.Sprintf("\n%s%s%s", border.BottomLeft, bottom, border.BottomRight))
 
-	return builder.String()
+	result := builder.String()
+
+	sep := lipgloss.NewStyle().Border(lipgloss.ThickBorder(), false, true, false, false).Height(ui.Height).Width(0)
+	result = lipgloss.JoinHorizontal(lipgloss.Top, result, sep.Render(""))
+
+	return result
 }
 
 func (m Model) Update(msg tea.Msg) (Model, tea.Cmd) {
