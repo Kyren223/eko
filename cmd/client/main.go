cc8eef8eb86aced564d8b4ff7eb5fd12e0595764
Kyren223 <Kyren223@proton.me>
2025-01-08 23:39:25 +0200

    Fixed CI, should deploy successfuly

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -7,7 +7,7 @@ import (
 	"github.com/kyren223/eko/internal/client"
 )
 
-func main(){
+func main() {
 	logFile, err := os.OpenFile("client.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0600)
 	if err != nil {
 		log.Fatalln(err)
