645247f4ef489c9084a7d8d30db89b083287f0c6
Kyren223 <Kyren223@proton.me>
2025-01-21 19:06:34 +0200

    format for ci

--- internal/packet/models.go
+++ internal/packet/models.go
@@ -1,14 +1,14 @@
 package packet
 
 const (
-	MaxNetworkNameBytes   = 32
-	MaxIconBytes          = 16
-	DefaultFrequencyName  = "main"
-	DefaultFrequencyColor = "#FFFFFF"
-	MaxFrequencyName      = 32
-	MaxUserDataBytes      = 8192
-	MaxMessageBytes       = 2000
-	MaxUsernameBytes = 32
+	MaxNetworkNameBytes     = 32
+	MaxIconBytes            = 16
+	DefaultFrequencyName    = "main"
+	DefaultFrequencyColor   = "#FFFFFF"
+	MaxFrequencyName        = 32
+	MaxUserDataBytes        = 8192
+	MaxMessageBytes         = 2000
+	MaxUsernameBytes        = 32
 	MaxUserDescriptionBytes = 200
 )
 
